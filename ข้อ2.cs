72689ed89f72c0dad0d4c30c8f403dcae7c735f3
Pongsakorn <magicsiam001@gmail.com>
2021-06-13 16:03:34 +0700

    ข้อ 2

---
+++ ข้อ2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace FinalExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Newvalue();
+        }
+        static void Newvalue()
+        {
+            Console.WriteLine("Input the amount of number: ");
+            int size = Convert.ToInt32(Console.In.ReadLine());
+            int[] Value = new int[size];
+
+            for (int i = 0; i < Value.Length; i++)
+            {
+
+                Value[i] = int.Parse(Console.ReadLine());
+
+                if (i == Value.Length)
+                {
+                    
+                    break;                   
+                }
+                
+               
+            }
+            Array.Sort(Value);
+            Console.Write("Tar's Sort ");            
+            foreach (int u in Value)
+            {
+
+                
+                Console.Write(u + " ");
+                
+            }
+            Console.WriteLine();
+            Console.Write("Rocky's Sort ");
+            foreach (int u in Value)
+            {
+
+
+                Console.Write(u + " ");
+
+            }
+            Console.WriteLine();
+            Console.Write("Peter's Sort ");
+            foreach (int u in Value)
+            {
+
+
+                Console.Write(u + " ");
+
+            }
+            Console.WriteLine();
+            Console.Write("Fluke's Sort ");
+            foreach (int u in Value)
+            {
+
+
+                Console.Write(u + " ");
+
+            }
+
+        }
+        
+    }
+}
+
